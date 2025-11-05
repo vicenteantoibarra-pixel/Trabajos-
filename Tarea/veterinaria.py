bdede3e18b2528358294380a41d5616240a7cb51
Vicente Ibarra <vicenteantoibarramail.com>
2025-11-05 10:18:00 -0300

    clas del 4/11

---
+++ Tarea/veterinaria.py
@@ -0,0 +1,9 @@
+from mascota import Mascotas
+
+class Veterinaria:
+    def __init__(self,nombreVeterinaria):
+        self.nombreVeterinaria = nombreVeterinaria
+        self.mascota = []
+
+    def mascotas (self,mascota:Mascotas):
+        
